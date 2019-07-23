0c8012dcdb0f753ef5b5d165816b86943cbcaa75
Przemyslaw Koziol <przemkoz85@gmail.com>
2019-07-23 02:40:43 +0200

    fix travis attempt 2

--- build/gnu/build.sh
+++ build/gnu/build.sh
@@ -1,3 +1,3 @@
-conan install -s arch=x86_64 -s compiler=gcc -s compiler.libcxx=libstdc++11 -s compiler.version=7 "../.." --build missing
-cmake -G "$CMAKE_GENERATOR" "../.."
+conan install -s arch=x86_64 --build missing
+cmake "../.."
 cmake --build .
---
+++ build/gnu/buildMSYS.sh
@@ -0,0 +1,3 @@
+conan install -s arch=x86_64 -s compiler=gcc -s compiler.libcxx=libstdc++11 -s compiler.version=7 "../.." --build missing
+cmake -G "MSYS Makefiles" "../.."
+cmake --build .
