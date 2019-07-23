9a741f8b5bbc976167ef89774d79db607fcd5deb
Przemyslaw Koziol <przemkoz85@gmail.com>
2019-07-23 03:02:43 +0200

    add travis status icon

---
+++ build/gnu/CMakeFiles/lfQueueTest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lfQueueTest.dir/src/tests/mainTests.cpp.obj"
+  "CMakeFiles/lfQueueTest.dir/src/tests/basic.cpp.obj"
+  "../../bin/lfQueueTest.pdb"
+  "../../bin/lfQueueTest.exe"
+  "../../bin/lfQueueTest.exe.manifest"
+  "liblfQueueTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lfQueueTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
