7106577333807263dbab4c2be2b3f2b8c1468564
Przemyslaw Koziol <przemkoz85@gmail.com>
2019-07-24 22:13:35 +0200

    asdsadsdsasasad

--- build/gnu/build.sh
+++ build/gnu/build.sh
@@ -1,3 +1,3 @@
-conan install -s compiler.libcxx=libstdc++11 "../.." --build missing
+conan install -s arch=x86_64 -s arch_build=x86_64 -s compiler=gcc -s compiler.libcxx=libstdc++ -s compiler.version=7  --build=missing ../..
 cmake "../.."
 cmake --build .
