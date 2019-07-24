bb83ed5d45fac8af035eadc9f4ef68ba7c255160
Przemyslaw Koziol <przemkoz85@gmail.com>
2019-07-24 22:50:20 +0200

    teedsf

--- build/gnu/build.sh
+++ build/gnu/build.sh
@@ -1,3 +1,3 @@
-conan install -s arch=x86_64 -s arch_build=x86_64 -s compiler=gcc -s compiler.libcxx=libstdc++ -s compiler.version=7  --build=missing ../..
+conan install ../.. --build=missing
 cmake "../.."
 cmake --build .
